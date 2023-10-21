374cd7984194c441704723494077c055c3eb87f3
Alexander Papchikhin <aleshmandr@gmail.com>
2023-10-21 11:02:19 +0200

    WIP

--- Packages/MacePhysics/Runtime/RaceTrackExtensions/TrackCache.cs
+++ Packages/MacePhysics/Runtime/RaceTrackExtensions/TrackCache.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Octree;
 using UnityEngine;
 
-namespace MR.RaceTrackExtensions
+namespace MRM.RaceTrackExtensions
 {
     public class TrackCache : MonoBehaviour
     {
